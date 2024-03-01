6b74f34feff6fc03d3b84de2e95569507d475e20
Meterel <114000274+Meterel@users.noreply.github.com>
2024-03-01 22:31:00 +0100

    Optimized load time

--- v/1.js
+++ v/1.js
@@ -2,7 +2,7 @@
 // https://github.com/Meterel/form-anti-spam
 
 
-export async function formAntiSpam({elem,exclude,form}){
+export function formAntiSpam({elem,exclude,form}){
     for(const x of document.querySelectorAll("script")) if(x.innerHTML.includes("formAntiSpam")) x.remove();
 
     let root;
@@ -12,22 +12,23 @@ export async function formAntiSpam({elem,exclude,form}){
         root=elem.lastElementChild;
         selector+=":not("+exclude+")";
     }else{
-        if(document.readyState!=="complete") await new Promise(r=>addEventListener("load",r));
-
         root=elem.attachShadow({mode:"closed"});
         root.innerHTML=form;
 
         const style=new CSSStyleSheet();
-        for(const s of document.styleSheets){
-            try{
-                for(const r of s.cssRules){
-                    try{
-                        style.insertRule(r.cssText,style.cssRules.length);
-                    }catch{}
-                }
-            }catch{}
-        }
         root.adoptedStyleSheets=[style];
+
+        function applyStyles(){
+            let styleText="";
+            for(const s of document.styleSheets){
+                try{
+                    for(const r of s.cssRules) styleText+=r.cssText;
+                }catch{}
+            }
+            style.replace(styleText);
+        }
+        if(document.readyState==="complete") applyStyles();
+        else addEventListener("load",applyStyles);
     }
 
     for(const e of [...root.querySelectorAll(selector),root]){
